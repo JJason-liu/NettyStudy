2d8856c0b8f3bd6769fc1621030519a68fc07899
JJason-liu <765798166@qq.com>
2019-03-06 17:54:42 +0800

    ..

--- src/main/java/netty_test/netty_test/myprogream/heart/AcceptorIdleStateTrigger.java
+++ src/main/java/netty_test/netty_test/myprogream/heart/AcceptorIdleStateTrigger.java
@@ -14,12 +14,15 @@ public class AcceptorIdleStateTrigger extends ChannelInboundHandlerAdapter {
 			switch(event.state()){
 			case ALL_IDLE:
 				System.out.println("读/写超时！！！");
+				ctx.channel().close();
 				break;
 			case READER_IDLE:
 				System.out.println("读超时！！！");
+				ctx.channel().close();
 				break;
 			case WRITER_IDLE:
 				System.out.println("写超时！！！");
+				ctx.channel().close();
 				break;
 			default:
 				break;
